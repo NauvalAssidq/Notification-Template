bb35e4d68970532b4f7508c80a8d833094c5edc2
NauvalAssidq <nauvalsidiq0427@gmail.com>
2025-04-22 11:08:49 +0700

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_PATH || "/Notification-Template",
-  build: {
-    lib: {
-      entry: "./src/main.tsx",
-      name: "ToastLibrary",
-      fileName: "toast-library",
-      formats: ["umd", "es"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
 });
